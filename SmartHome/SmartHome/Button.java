c17d65b4c27eaa1a291b73e539870ec5a2c5ec2a
Daniel Al Banna <165435551+Daniel-Banna@users.noreply.github.com>
2026-01-07 18:19:02 +0100

    Add Button class extending PowerDevice

---
+++ SmartHome/SmartHome/Button.java
@@ -0,0 +1,13 @@
+package smarthome;
+
+public class Button extends PowerDevice {
+	
+	public Button(Device device) {
+		super(device);
+	}	
+	
+	@Override
+	public void poll() {
+		getDevice().run();
+	}
+}
